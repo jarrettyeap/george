019b22996728c2c0df95e39c919fbb47f11a1101
Yeap Hooi Tong <hooitongx@gmail.com>
2017-01-18 14:34:09 +0800

    Initial bootstrapping of project

---
+++ app/src/main/java/co/realityshifters/george/Question.java
@@ -0,0 +1,12 @@
+package co.realityshifters.george;
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
